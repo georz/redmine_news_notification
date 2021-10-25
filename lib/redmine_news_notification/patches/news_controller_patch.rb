17b49d174cf6464740bc10c21dd0a2cccf671e09
Liane Hampe <liaham@xmera.de>
2021-10-25 16:33:24 +0200

    Add lisence text

--- lib/redmine_news_notification/patches/news_controller_patch.rb
+++ lib/redmine_news_notification/patches/news_controller_patch.rb
@@ -1,3 +1,24 @@
+# frozen_string_literal: true
+
+# This file is part of the Plugin Redmine News Notification.
+#
+# Copyright (C) 2021 Liane Hampe <liaham@xmera.de>, xmera.
+# Copyright (C) 2013 - 2016 George Harada <http://georz.hatenablog.com/>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
 module RedmineNewsNotification
   module Patches
     module NewsControllerPatch
@@ -9,15 +30,17 @@ module RedmineNewsNotification
         def show
           current_user_id = User.current.id
           read_check = NewsNotification.where(
-            :user_id => current_user_id,
-            :news_id => @news.id
-            ).take
+            user_id: current_user_id,
+            news_id: @news.id
+          ).take
 
-          NewsNotification.create(
-            :user_id => current_user_id,
-            :news_id => @news.id,
-            :read_on => Time.now.to_s(:db)
-            ) if read_check.nil?
+          if read_check.nil?
+            NewsNotification.create(
+              user_id: current_user_id,
+              news_id: @news.id,
+              read_on: Time.now.to_s(:db)
+            )
+          end
           super
         end
       end
